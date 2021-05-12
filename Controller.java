371fd4a26fb7df9b8c3ffbe44e2615c4eebe1c9f
Yehor <zapara_2012@ukr.net>
2021-05-12 16:09:20 +0300

    Online chat which use objects

--- Controller.java
+++ Controller.java
@@ -2,33 +2,59 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Controller {
+    Socket socket;
+    DataOutputStream out;
+    @FXML
+    TextArea textAreaUserList;
     @FXML
     TextArea textArea;
+    @FXML
+    TextField textField;
+    @FXML
+    private void send(){
+        String text = textField.getText();
+        try {
+            out.writeUTF(text);
+            textField.clear();
+            textField.requestFocus();
+            textArea.appendText("Вы: "+text+"\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @FXML
-    private void connect() {
+    private void connect(){
         try {
-            Socket socket = new Socket("192.168.0.107", 8188);
-            DataInputStream in = new DataInputStream(socket.getInputStream());
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            socket = new Socket("localhost",8188);
+            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
             Thread thread = new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    while (true) {
-                        String response = null; // Читаем ответ сервера
+                    while (true){
                         try {
-                            response = in.readUTF();
-                            textArea.appendText(response + "\n");
-                            System.out.println(response);
-                        } catch (IOException e) {
+                            String response = ois.readObject().toString(); // Ждём сообщение от сервера
+                            if(response.indexOf("**userList**") == 0){
+                                String[] usersName= response.split("//");
+                                textAreaUserList.clear();
+                                for (String userName:usersName) {
+                                    textAreaUserList.appendText(userName+"\n");
+                                }
+                            }else{
+                                textArea.appendText(response+"\n");
+                            }
+                        } catch (Exception e) {
                             e.printStackTrace();
                         }
                     }
@@ -36,22 +62,6 @@ public class Controller {
                 }
             });
             thread.start();
-            Thread thread1 = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    while (true) {
-                        Scanner scanner = new Scanner(System.in);
-                        String request = scanner.nextLine();
-                        try {
-                            out.writeUTF(request); // Отправляем сообщение серверу
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            });
-            thread1.start();
-
         } catch (IOException e) {
             e.printStackTrace();
         }
--- Main.java
+++ Main.java
@@ -1,10 +1,12 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 
 public class Main extends Application {
 
@@ -12,8 +14,12 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Сетевой чат");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 400, 300));
         primaryStage.show();
+        primaryStage.setOnCloseRequest(e->{
+            Platform.exit();
+            System.exit(0);
+        });
     }
 
 
@@ -21,3 +27,12 @@ public class Main extends Application {
         launch(args);
     }
 }
+
+
+
+/* --module-path
+"C:\Program Files\Java\javafx-sdk-11.0.2\lib"
+--add-modules
+javafx.controls,javafx.fxml,javafx.base,javafx.graphics,javafx.web
+--add-exports
+javafx.graphics/com.sun.javafx.sg.prism=ALL-UNNAMED */
